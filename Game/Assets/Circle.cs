1673655e7e6fd4b202630d227030420abb9b0eb8
nordberg <marcusnordberg@live.se>
2015-02-22 16:14:26 +0100

    Grund till Tetris i Unity

---
+++ Game/Assets/Circle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Circle : MonoBehaviour {
+
+	public float gravity = 0.01f * 9.82f;
+
+	public Vector3 velocity;
+
+	// Use this for initialization
+	void Start () {
+		velocity = new Vector3(0, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		velocity += new Vector3(0, -gravity, 0);
+		transform.position += velocity;
+	}
+}
---
+++ Game/Assets/Group.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Group : MonoBehaviour {
+
+	float gravity = 0.0001f * 9.82f;
+
+	bool isValidGridPos() {
+		foreach (Transform child in transform) {
+			Vector2 v = Grid.roundVec2(child.position);
+			
+			// Not inside Border?
+			if (!Grid.insideBorder(v))
+				return false;
+			
+			// Block in grid cell (and not part of same group)?
+			if (Grid.grid[(int)v.x, (int)v.y] != null &&
+			    Grid.grid[(int)v.x, (int)v.y].parent != transform)
+				return false;
+		}
+		return true;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			transform.Rotate (0, 0, -90);
+
+			if (isValidGridPos()) {
+				updateGrid();
+			} else {
+				transform.Rotate (0, 0, 90);
+			}
+		} else if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			transform.position += new Vector3(-1, 0, 0);
+			if (isValidGridPos()) {
+				updateGrid();
+			} else {
+				transform.position += new Vector3(1, 0, 0);
+			}
+		} else if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			transform.position += new Vector3(1, 0, 0);
+			if (isValidGridPos()) {
+				updateGrid();
+			} else {
+				transform.position += new Vector3(-1, 0, 0);
+			}
+		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			transform.position += new Vector3(0, -1, 0);
+
+			if (isValidGridPos()) {
+				updateGrid();
+			} else {
+				transform.position += new Vector3(0, 1, 0);
+
+				FindObjectOfType<Spawner>().spawnNext();
+
+				enabled = false;
+			}
+		}
+	}
+			
+	void updateGrid() {
+		// Remove old children from grid
+		for (int y = 0; y < Grid.h; ++y)
+			for (int x = 0; x < Grid.w; ++x)
+				if (Grid.grid[x, y] != null)
+					if (Grid.grid[x, y].parent == transform)
+						Grid.grid[x, y] = null;
+		
+		// Add new children to grid
+		foreach (Transform child in transform) {
+			Vector2 v = Grid.roundVec2(child.position);
+			Grid.grid[(int)v.x, (int)v.y] = child;
+		}        
+	}
+}
